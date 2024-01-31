ca0ee0d59db1c36470e073369adf64bfdfe81be1
Oscar Orellana <oscarore284@gmail.com>
2024-01-31 16:36:59 -0600

    test

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
 
-let detector = new MobileDetect(window.navigator.userAgent);
-
-alert(`
-
-Mobile = ${detector.mobile()}
-Phone = ${detector.phone()}
-Tablet = ${detector.tablet()}
-`)
-
 // Detección del dispositivo
 // alert(navigator.userAgent)
 
 // alert(getBrand())
 
-function getBrand() {
-    const userAgent = navigator.userAgent;
-    const brands = [
-        { name: 'Apple', identifier: 'iPhone|iPad|iPod' },
-        { name: 'Samsung', identifier: 'SAMSUNG|SM-' },
-        { name: 'Google', identifier: 'Pixel' },
-        // Add more brands and their respective identifiers as needed
-    ];
+alert(getAppStoreUrl());
 
-    for (const brand of brands) {
-        const regex = new RegExp(brand.identifier, 'i');
-        if (regex.test(userAgent)) {
-            return brand.name;
-        }
+function getAppStoreUrl() {
+    var userAgent = navigator.userAgent || window.opera;
+    if (/huawei/i.test(userAgent)) {
+        return "https://my-link.com";
     }
-
-    return 'Unknown';
-
-
+    if (/android/i.test(userAgent)) {
+        return "https://play.google.com";
+    }
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "https://apps.apple.com";
+    }
+    return null;
 }
+
 
 // let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 // scanner.addListener('scan', function (content) {
