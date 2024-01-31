7f616a66243bd4aee9d598c5b357c30e228eff54
Oscar Orellana <oscarore284@gmail.com>
2024-01-31 15:29:25 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Detección del dispositivo
+alert(navigator.userAgent)
+
+let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+scanner.addListener('scan', function (content) {
+    console.log(content);
+});
+Instascan.Camera.getCameras().then(function (cameras) {
+    if (cameras.length > 0) {
+        scanner.start(cameras[0]);
+    } else {
+        console.error('No cameras found.');
+    }
+}).catch(function (e) {
+    console.error(e);
+});
